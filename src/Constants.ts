ba2647519498eb89d827d9e262c8b6dde9644683
jiri3 <>
2020-06-21 16:19:04 +0900

    [update]定数をまとめる

---
+++ src/Constants.ts
@@ -0,0 +1,3 @@
+export const MILLI = 0.001;
+export const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+export const CREDENTIALS_PATH = "./credentials.json";
--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ import OAuthForGoogleApi from "./OAuthForGoogleApi";
 import GmailApi from "./GmailApi";
 import * as repl from "repl";
 import * as vm from "vm";
-
-const MILLI = 0.001;
-const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
-const CREDENTIALS_PATH = "./credentials.json";
+import * as Constants from "./Constants";
 
 // 当時のAM 9:00に設定する
 let timeCondition = new Date();
 timeCondition.setUTCHours(0, 0, 0, 0);
 timeCondition.setDate(26);
 
-const aOuth = new OAuthForGoogleApi(SCOPES, CREDENTIALS_PATH);
+const aOuth = new OAuthForGoogleApi(
+    Constants.SCOPES,
+    Constants.CREDENTIALS_PATH
+);
 
 const replOption: repl.ReplOptions = {
     eval: commandEval,
@@ -33,13 +33,13 @@ function commandEval(
             let param: gmail_v1.Params$Resource$Users$Messages$List = {
                 userId: `me`,
                 q: `from:tradesys@rakuten-sec.co.jp after:${
-                    timeCondition.getTime() * MILLI
+                    timeCondition.getTime() * Constants.MILLI
                 }`,
                 // q: `from:tradesys@rakuten-sec.co.jp after:2020/05/07 before:2020/05/08`,
                 //q:`after:2020/5/4 before:2020/5/7 subject:What`,
             };
             console.log(param);
-            const searchTime = Math.floor(Date.now() * MILLI); //ms以下は切り捨てる
+            const searchTime = Math.floor(Date.now() * Constants.MILLI); //ms以下は切り捨てる
             // TODO await使いたい
             const messages = GmailApi.getMessages(aOuth, param);
             messages.then((value) => {
@@ -51,7 +51,7 @@ function commandEval(
             });
             break;
         case "end":
-            aOuth.revoke();
+            // aOuth.revoke();
             replServer.close();
             break;
         case "":
